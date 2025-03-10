8a698ce772b8929680c247b5fa395e27538fcb3d
Valentino <valebtinbest@gmail.com>
2025-03-10 17:06:13 +0100

    PAN-85

--- src/agents/follow_unfollow_bot/src/follow_unfollow_bot/config/config.py
+++ src/agents/follow_unfollow_bot/src/follow_unfollow_bot/config/config.py
@@ -22,6 +22,7 @@ class Config:
         DB_HOST = os.getenv("DB_HOST", "localhost")
         DB_PORT = os.getenv("DB_PORT", "5432")
         DB_NAME = os.getenv("DB_NAME", "postgres")
+        URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
     class FASTAPI:
         ALLOWED_ORIGINS = [
