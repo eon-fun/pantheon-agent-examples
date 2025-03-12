2aa06a66cb47b90e767fa6827a57b4abc685e755
Valentino <valebtinbest@gmail.com>
2025-03-12 20:16:18 +0100

    PAN-78 (redo logic of bot)

--- src/agents/twitter-echo-bot/src/twitter_echo_bot/main.py
+++ src/agents/twitter-echo-bot/src/twitter_echo_bot/main.py
@@ -1,8 +1,20 @@
 import asyncio
+from contextlib import asynccontextmanager
+from typing import List
 
-from DB.sqlalchemy_database_manager import init_models
+from fastapi import FastAPI, APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from starlette.middleware.cors import CORSMiddleware
+from base_agent.ray_entrypoint import BaseAgent
 
-from custom_logs.custom_logs import log
+from routers.tracked_accounts import tracked_accounts_router
+from routers.user import user_router
+from config.config import config
+
+
+from DB.sqlalchemy_database_manager import init_models, get_db
+from ray import serve
+from agents_tools_logger.main import log
 from tools.create_tweets_service import CreateTweetsService
 from tools.get_tweets_service import TwitterCollectorClient
 
@@ -11,7 +23,7 @@ async def init_app():
     await init_models()
 
 
-async def main():
+async def run_background_task():
     await init_app()
 
     async def start_collecting_tweets():
@@ -39,9 +51,46 @@ async def main():
 
     await asyncio.gather(*tasks)
 
+@asynccontextmanager
+async def lifespan(application: FastAPI):
+    log.info("Application startup")
+    await run_background_task()
+    yield
+    log.info("Application shutdown")
+
+
+ROUTERS: List[APIRouter] = [user_router,
+                            tracked_accounts_router]
+
+
+app = FastAPI(
+    lifespan=lifespan,
+    title=config.app_title,
+    description=config.app_description,
+    version=config.app_version,
+    docs_url=f"/{config.app_docs_url}/docs",
+    redoc_url=f"/{config.app_docs_url}/redoc",
+    openapi_url=f"/{config.app_docs_url}/openapi.json",
+)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=config.fastapi.allowed_origins,
+    allow_credentials=config.fastapi.allowed_credentials,
+    allow_methods=config.fastapi.allowed_methods,
+    allow_headers=config.fastapi.allowed_headers,
+)
+for router in ROUTERS:
+    app.include_router(router)
+
+
+
+@serve.deployment
+@serve.ingress(app)
+class FollowUnfollowBot(BaseAgent):
+    @app.post("/{goal}")
+    async def handle(self, goal: str, plan: dict | None = None,
+                     session: AsyncSession = Depends(get_db)):
+        pass
 
 if __name__ == "__main__":
-    try:
-        asyncio.run(main())
-    except KeyboardInterrupt:
-        log.info("Программа остановлена вручную.")
+    serve.run(app, route_prefix="/")
