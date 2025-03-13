34131317100f02aed5eecf5ae7ed06cd946d5e77
Valentino <valebtinbest@gmail.com>
2025-03-13 20:52:32 +0100

    PAN-78 (add postgre in config.py)

--- src/agents/twitter-echo-bot/src/twitter_echo_bot/config/config.py
+++ src/agents/twitter-echo-bot/src/twitter_echo_bot/config/config.py
@@ -21,15 +21,15 @@ class FastAPISettings(BaseSettings):
 
 
 class DatabaseSettings(BaseSettings):
-    db_user: str = Field(..., env="DB_USER")
-    db_password: str = Field(..., env="DB_PASSWORD")
-    db_host: str = Field(..., env="DB_HOST")
-    db_port: str = Field(..., env="DB_PORT")
-    db_name: str = Field(..., env="DB_NAME")
+    db_user: str = Field(..., env="DB_USER", default="rag")
+    db_password: str = Field(..., env="DB_PASSWORD", default="rag")
+    db_host: str = Field(..., env="DB_HOST", default="localhost")
+    db_port: str = Field(..., env="DB_PORT", default="5432")
+    db_name: str = Field(..., env="DB_NAME", default="rag")
+
     @property
     def url(self) -> str:
         return f"postgresql+asyncpg://{self.db_user}:{self.db_password}@{self.db_host}:{self.db_port}/{self.db_name}"
-
 
 
 class AppConfig(BaseSettings):
