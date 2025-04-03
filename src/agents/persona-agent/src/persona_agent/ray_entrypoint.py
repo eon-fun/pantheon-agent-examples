557e829ea41dce625173092d6f87b0007ce9a645
mdmeldon <mdmeldon@gmail.com>
2025-04-03 03:14:09 +0300

    add simple agent logic

--- src/agents/persona-agent/src/persona_agent/ray_entrypoint.py
+++ src/agents/persona-agent/src/persona_agent/ray_entrypoint.py
@@ -3,6 +3,11 @@ from contextlib import asynccontextmanager
 from base_agent.ray_entrypoint import BaseAgent
 from fastapi import FastAPI
 from ray import serve
+from jinja2 import Environment, PackageLoader, select_autoescape
+
+from qdrant_client_custom.main import get_qdrant_client
+from redis_client.main import get_redis_db
+from send_openai_request.main import get_embedding, send_openai_request
 
 
 @asynccontextmanager
@@ -12,6 +17,7 @@ async def lifespan(app: FastAPI):
     # handle clean up
 
 app = FastAPI(lifespan=lifespan)
+
 
 @serve.deployment
 class SubAgent:
@@ -27,15 +33,60 @@ class SubAgent:
 @serve.deployment
 @serve.ingress(app)
 class ExampleAgent(BaseAgent):
-    @app.post("/{goal}")
-    async def handle(self, goal: str, plan: dict | None = None):
-        return self.get_persona_template(goal)
-    
-    async def get_persona_template(self, goal: str):
-        
-        return super().get_persona_template(goal, plan)
-    
+    def __init__(self):
+        super().__init__()
+        self.jinja_env = Environment(
+            loader=PackageLoader("persona_agent", "templates"),
+            autoescape=select_autoescape()
+        )
 
+    @app.post("/{goal}")
+    async def handle(self, goal: str, plan: dict | None = None, prompt: str = ""):
+        return await self.get_persona_template(goal, prompt)
+
+    async def get_persona_template(self, goal: str, prompt: str):
+        redis_db = get_redis_db()
+        qdrant_client = get_qdrant_client()
+        persona_collection = goal
+
+        # заменить на проверку not exists в redis
+        if not persona_collection:
+            return "No persona collection found"
+
+        desc_key = f"{goal}:description"
+        persona_description = redis_db.get(desc_key) or ""
+
+        embedding_input = await get_embedding.remote(prompt)
+
+        search_similar_tweets = qdrant_client.search(
+            collection_name=persona_collection,
+            query_vector=embedding_input,
+            limit=5
+        )
+        similar_tweets = [tweet["text"] for tweet in search_similar_tweets]
+        context = "\n".join(similar_tweets)
+
+        # Используем Jinja2 для рендеринга шаблонов
+        template = self.jinja_env.get_template(
+            "prompts/generation_prompt.txt.j2")
+        generation_prompt = template.render(
+            persona_description=persona_description,
+            context=context,
+            prompt=prompt
+        )
+
+        # Также используем Jinja2 для системного сообщения
+        system_template = self.jinja_env.get_template(
+            "prompts/system_message.txt.j2")
+        system_message = system_template.render()
+
+        messages = [
+            {"role": "system", "content": system_message},
+            {"role": "user", "content": generation_prompt}
+        ]
+        result = await send_openai_request(messages=messages, temperature=0.7)
+
+        return result
 
 
 # serve run entrypoint:app
