e2b040c3b9d0eb193b917ccec702cad1ad8ec5e2
mdmeldon <mdmeldon@gmail.com>
2025-04-29 19:36:33 +0300

    add chart

--- src/agents/kol-agent/src/kol_agent/config.py
+++ src/agents/kol-agent/src/kol_agent/config.py
@@ -8,9 +8,9 @@ class Config(BaseSettings):
     REPLY_PERCENTAGE: float = 0.6
     RETWEET_PERCENTAGE: float = 0.5
 
-    LANGFUSE_SECRET_KEY: str = "sk-lf-03687f3f-9cc8-4661-b1a4-acf432360552"
-    LANGFUSE_PUBLIC_KEY: str = "pk-lf-61839399-0194-403e-9d21-6e7dcd3f5041"
-    LANGFUSE_HOST: str = "https://langfuse.dev.pntheon.ai"
+    LANGFUSE_SECRET_KEY: str
+    LANGFUSE_PUBLIC_KEY: str
+    LANGFUSE_HOST: str
 
 
 @lru_cache
